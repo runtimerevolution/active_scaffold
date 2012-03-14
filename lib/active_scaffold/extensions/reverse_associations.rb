70cb5fafce4c0e6fa50a7346909151d0ced32778
heliomgg <h.gomes@karma-consulting.pt>
2012-03-14 12:18:53 +0000

    Fixed problem with associations that have namespaces.

--- lib/active_scaffold/extensions/reverse_associations.rb
+++ lib/active_scaffold/extensions/reverse_associations.rb
@@ -31,7 +31,7 @@ module ActiveRecord
             else
               # skip over has_many :through associations
               next if assoc.options[:through]
-              next unless assoc.options[:polymorphic] or assoc.class_name.constantize == self.active_record
+              next unless assoc.options[:polymorphic] or assoc.klass == self.active_record
 
               case [assoc.macro, self.macro].find_all{|m| m == :has_and_belongs_to_many}.length
                 # if both are a habtm, then match them based on the join table
